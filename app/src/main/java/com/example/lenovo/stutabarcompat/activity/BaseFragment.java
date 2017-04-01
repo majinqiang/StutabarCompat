9ac1bc5cc8960bee490e382643b102cc2ca0324b
majinqing <ma163jinqiang@163.com>
2017-04-01 13:17:30 -0700

    第二次提交

---
+++ app/src/main/java/com/example/lenovo/stutabarcompat/activity/BaseFragment.java
@@ -0,0 +1,30 @@
+package com.example.lenovo.stutabarcompat.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by lenvo on 2017/4/1.
+ */
+
+public class BaseFragment extends Fragment {
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
